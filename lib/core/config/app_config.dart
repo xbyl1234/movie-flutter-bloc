ecb6e6e1117dd7500a16febd84e2394697e2dd60
AMB-Truong <truong.pham@amoeba.group>
2024-12-27 17:26:01 +0700

    add firebase

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:movie/di/dependency_injection.dart' as di;
 import 'package:flutter/material.dart';
 
 Future<void> appConfig() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await di.init();
 }
